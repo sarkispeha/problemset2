8e668f2f4574c953d57402df773d637bdf62709f
Sarkis Peha <sarkispeha@gmail.com>
2014-09-25 17:30:40 -0600

    getting lost...

--- main.js
+++ main.js
@@ -32,19 +32,42 @@ var swapCase = function (str) {
 //////////////////////////////////////
 var letterCount = function(str) {
 	var splitToWords = str.split(' ');
-	console.log(splitToWords);
-
+	// console.log(splitToWords);
 
 	for(var i = 0; i < splitToWords.length; i++) {
 
 		var currentPalabra = splitToWords[i];
-		console.log(currentPalabra);
+		// console.log(currentPalabra);
+		var letterPool = [];
+		var multipleLetWrdPool = [];
+		var mostLetrWord = '';
 
 		for(var j = 0; j < currentPalabra.length; j++) {
 			
-			var letterPool = [];
 			var currentLetra = currentPalabra[j];
-			console.log(currentLetra);
+			// console.log(currentLetra);
+			if (letterPool.indexOf(currentLetra) === -1) {
+				letterPool.push(currentLetra);
+				// console.log(letterPool);
+			}else{
+				// console.log(currentPalabra);
+				multipleLetWrdPool.push(currentPalabra);
+				console.log(multipleLetWrdPool);
+
+			}
 		};
+		// for(var k = 0; k < multipleLetWrdPool.length; k++) {
+		// 	var mostLetters = 0;
+		// 	// var mostLetrWord = '';
+		// 	if (multipleLetWrdPool[k].length > mostLetters){
+		// 		console.log(multipleLetWrdPool[k]);
+		// 		mostLetters = multipleLetWrdPool[k].length;
+		// 		mostLetrWord = multipleLetWrdPool[k];
+		// 	}
+		// }
+		// console.log(mostLetters);
+		
+
 	};
+
 };
