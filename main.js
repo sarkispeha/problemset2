0d86dbdc2440ae904a861f4e78308aed1cb7c1f9
Sarkis Peha <sarkispeha@gmail.com>
2014-09-25 11:53:04 -0600

    problem 1 complete

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 $(document).on('ready', function() {
   
 });
+
+var firstReverse = function(str){
+	var finalArray =[];
+	var arr = str.split('');
+
+	for( var i = str.length; i >= 0; i--) {
+		finalArray.push(arr[i]);
+	}
+	finalArray = finalArray.join('');
+	console.log(finalArray)
+	return finalArray;
+}
